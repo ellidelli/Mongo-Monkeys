27164c64d6fb6e01e5757d55054f8522afd55974
ellidelli <elizabeth.delea@gmail.com>
2024-07-12 15:17:23 -0400

    home page

---
+++ react/src/componenets/EmployeeCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './EmployeeCard.css'
+
+const EmployeeCard = (props) => {
+    return (
+        <>
+            <div className="employee-card">
+                <div ClassName="employee-picture">
+                    
+                </div>
+                <div ClassName="employee-data">
+                    <p>Name: {props.data.name}</p>
+                    <p>Phone Number: {props.data.phone}</p>
+                    <p>Job: {props.data.job_role}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default EmployeeCard
--- react/src/componenets/Home.jsx
+++ react/src/componenets/Home.jsx
@@ -1,9 +1,77 @@
 import React from "react";
+import { useEffect, useState } from 'react'
+import EmployeeCard from "./EmployeeCard";
+import './EmployeeCard.css'
+import Monkey from '../assets/MongoMonkey.png'
 
-const Home = () => {
-    return(
+const Home = (props) => {
+    const [images, setImages] = useState([]);
+    useEffect(() => {
+        //todo create an image getter
+        //https://randomuser.me/api/portraits/men/53.jpg
+    })
+
+    const HRSpecialist = props.data.filter(employee => employee.job_role === 'HR Specialist');
+    const Designer = props.data.filter(employee => employee.job_role === 'Designer');
+    const ProductManager = props.data.filter(employee => employee.job_role === 'Product Manager');
+    const DataAnalyst = props.data.filter(employee => employee.job_role === 'Data Analyst');
+    const SoftwareEngineer = props.data.filter(employee => employee.job_role === 'Software Engineer');
+
+    return (
+
         <>
-        <h1>Home</h1>
+            <div className="home">
+                <img src={Monkey} alt="" />
+                <h1>MongoMonkey Employee Directory</h1>
+            </div>
+
+            <h3>HR</h3>
+            <div className="employees">
+                {
+                    HRSpecialist.map((employee) => (
+                        <EmployeeCard key={employee.employee_id} data={employee} />
+                    ))
+                }
+            </div>
+
+            <h3>Product Managers</h3>
+            <div className="employees">
+                {
+                    ProductManager.map((employee) => (
+                        <EmployeeCard key={employee.employee_id} data={employee} />
+                    ))
+                }
+            </div>
+
+            <h3>Softtware Engineers</h3>
+            <div className="employees">
+                {
+                    SoftwareEngineer.map((employee) => (
+                        <EmployeeCard key={employee.employee_id} data={employee} />
+                    ))
+                }
+            </div>
+
+            <h3>Data Analysts</h3>
+            <div className="employees">
+                {
+                    DataAnalyst.map((employee) => (
+                        <EmployeeCard key={employee.employee_id} data={employee} />
+                    ))
+                }
+            </div>
+
+            <h3>Designers</h3>
+            <div className="employees">
+                {
+                    Designer.map((employee) => (
+                        <EmployeeCard key={employee.employee_id} data={employee} />
+                    ))
+                }
+            </div>
+
+
+
         </>
     )
 }
