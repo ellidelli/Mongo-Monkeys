c650650f7a8b19cae48bb4bd179e3a9a1dfccc3d
ellidelli <elizabeth.delea@gmail.com>
2024-07-12 11:33:13 -0400

    server.js connects to the database

---
+++ server/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// Load environment variables
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION; 
+
+// Create an instance of Express
+const app = express();
+const PORT = 3000;
+
+// Use CORS and JSON middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/employees', async (req, res) => {
+    let client;
+
+    try {
+        // Connect to the MongoDB client
+        client = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const employees = await collection.find({}).toArray();
+        res.json(employees);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("No employees");
+    }
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
