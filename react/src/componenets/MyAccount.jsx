af4570d2e9c7f3e417f56af67efd74fe5e6d78fb
c-drew <andrew.cavar@hotmail.com>
2024-07-16 09:49:53 -0400

    added numer formatting to employee cards

--- react/src/componenets/MyAccount.jsx
+++ react/src/componenets/MyAccount.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import ManageSection from "./ManageSection";
 
 const MyAccount = () => {
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFractionDigits: 0
+    });
+
     return (
         <>
             <h1>{localStorage.getItem('name')}</h1>
             <h3>📍 {localStorage.getItem('work_location')}</h3>
             <h3>Role: {localStorage.getItem('job_role')}</h3>
-            <h3>Salary: {localStorage.getItem('salary')}</h3>
+            <h3>Salary: {formatter.format(localStorage.getItem('salary'))}</h3>
             <ManageSection></ManageSection>
             
         </>
--- react/src/componenets/SalEmpCard.jsx
+++ react/src/componenets/SalEmpCard.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import './EmployeeCard.css'
 
 const SalEmpCard = (props) => {
+
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFractionDigits: 0
+    });
+
     return (
         <>
             <div className="employee-card">
@@ -10,9 +17,9 @@ const SalEmpCard = (props) => {
                 </div>
                 <div className="employee-data">
                     <p>Name: {props.data.name}</p>
-                    <p>Phone Number: {props.data.phone}</p>
+                    <p>Phone Number: {('('+props.data.phone.toString().substring(0, 3)+') '+props.data.phone.toString().substring(3, 6)+'-'+props.data.phone.toString().substring(6, 10))}</p>
                     <p>Job: {props.data.job_role}</p>
-                    <p>Salary: {props.data.salary}</p>
+                    <p>Salary: {formatter.format(props.data.salary)}</p>
                 </div>
             </div>
         </>
